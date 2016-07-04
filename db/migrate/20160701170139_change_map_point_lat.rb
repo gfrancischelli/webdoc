a674e0dee68ff12c0cc0410217606d39fc006118
Giovani Francischelli <gifrancischelli@gmail.com>
2016-07-04 15:02:35 -0300

    Removed map_points table

--- db/migrate/20160701170139_change_map_point_lat.rb
+++
@@ -1,6 +0,0 @@
-class ChangeMapPointLat < ActiveRecord::Migration
-  def change
-  	execute 'ALTER TABLE map_points ALTER COLUMN lat TYPE integer USING (lat::integer)'
-  	execute 'ALTER TABLE map_points ALTER COLUMN lng TYPE integer USING (lng::integer)'
-  end
-end
