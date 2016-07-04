2e52c8683dc22db3af6e781e6158d7a1f60b3312
Giovani Francischelli <gifrancischelli@gmail.com>
2016-07-04 16:50:00 -0300

    WebdocMap() init

--- app/assets/javascripts/player/player_initialize.js
+++ app/assets/javascripts/player/player_initialize.js
@@ -1,15 +1,61 @@
+function WebdocPlayer() {
+
+    var url  = $('#player-container').data('video-url');
+    var video_id = 1;
+    var player = { 
+        playing: false,
+        youtube: {}
+    }
+
+    var episode = {}
+
+    function init() {
+        console.log('init');
+        fetch('/videos/' + video_id + '.json')
+            .then(function(response) { return response.json(); })
+            .then(function(episode_json) {
+                episode = episode_json;
+
+                youtubeGen(url)
+            });
+    }
+
+    function youtubeGen(url) {
+        player.youtube = new YT.Player('youtube', {
+            videoId: url,
+            playerVars: { fs: false },
+            events: {
+            //  Relative to PLAYER BUTTONS -------
+            // 'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    function onPlayerStateChange(event) {
+        if (event.data == 1) {
+            if (!player.playing) {
+                $("#main-navbar").fadeToggle()
+                player.playing = true
+            }
+        }
+
+        if (event.data == 2) {
+            $("#main-navbar").fadeToggle()
+            player.playing = false
+        }
+    }
+
+
+    var publicAPI = {
+        init: init()
+    }
+
+    return { publicAPI }
+}
+
 
 function onYouTubeIframeAPIReady() {
-  var url =  $('#player-container').data('video-url')
-  var player;
-  player = new YT.Player('youtube', {
-    videoId: url,
-    playerVars: {
-      fs: false },
-    events: {
-      //  Relative to PLAYER BUTTONS -------
-      // 'onReady': onPlayerReady,
-      'onStateChange':  window.onPlayerStateChange
-    }
-  });
+    console.log('onYoutubeAPIReady')
+    WebdocPlayer().init()
 }
--- app/views/videos/show.json.jbuilder
+++ app/views/videos/show.json.jbuilder
@@ -1,2 +1,17 @@
 json.id @video.id
 json.title @video.title
+json.posts @video.video_posts do |post|
+	json.title post.title
+	json.content post.content
+	json.cooX = post.cooX
+	json.cooY = post.cooY
+	json.fade_in = post.fade_in
+	json.fade_out = post.fade_out
+	end
+json.map_posts @video.map_posts do |post|
+	json.title post.title
+	json.cooX = post.cooX
+	json.cooY = post.cooY
+	json.fade_in = post.fade_in
+	json.fade_out = post.fade_out
+	end
