c91b031e85de19bf3b112c4981717cc5c7b07bd5
Giovani Francischelli <gifrancischelli@gmail.com>
2016-07-07 16:00:04 -0300

    remove const (es6)

--- app/assets/javascripts/map/interactive_map.js
+++ app/assets/javascripts/map/interactive_map.js
@@ -1,91 +1,91 @@
-// function WebdocMap() {
+function WebdocMap() {
 
-// 	// Sao Paulo
-// 	const map_coordinates = { lat: -22.373416, lng: -48.412382 };
+	// Sao Paulo
+	var map_coordinates = { lat: -22.373416, lng: -48.412382 };
 
-// 	// JSON array with ( rb ).MapPost.all.json
-// 	// After marksGen() every post inside map_posts
-// 	// has a mark (google maps mark)
-// 	var map_posts;
+	// JSON array with ( rb ).MapPost.all.json
+	// After marksGen() every post inside map_posts
+	// has a mark (google maps mark)
+	var map_posts;
 
-// 	// Episodes_url = Video.all.json
-// 	// Call mapGen() then marksGen()
-// 	function init() {
-// 		fetch('/map_posts.json')
-// 			.then(function(response) { 
-// 				return response.json();
-// 			})
-// 			.then(function(marks_json) {
-// 				map_posts = marks_json;
-// 			    mapGen();
-// 				marksGen();
-// 			});
-// 	}
-
-
-// 	//  Embed Map in #map-embed element
-// 	//  Then responsive resize event listener
-// 	function mapGen() {
-// 		map = new google.maps.Map(document.getElementById('map-embed'), {
-// 		  zoom: 7,
-// 		  center: map_coordinates,
-// 		  mapTypeId: google.maps.MapTypeId.HYBRID,
-// 		  scrollwheel: false
-// 		});
-
-// 		google.maps.event.addDomListener(window, "resize", function() {
-// 		  var center;
-// 		  center = map.getCenter();
-// 		  google.maps.event.trigger(map, "resize");
-// 		  map.setCenter(center);
-// 		});
-// 	}
+	// Episodes_url = Video.all.json
+	// Call mapGen() then marksGen()
+	function init() {
+		fetch('/map_posts.json')
+			.then(function(response) { 
+				return response.json();
+			})
+			.then(function(marks_json) {
+				map_posts = marks_json;
+			    mapGen();
+				marksGen();
+			});
+	}
 
 
-// 	//  For each post in map_post
-// 	//  create and store a new Google Maps Marker
-// 	function marksGen() { 
-// 		map_posts.forEach(function(point) {
-// 			var this_point = point;
-// 			this_point.mark = new google.maps.Marker({
-// 			  position: point.coordinates,
-// 			  title: point.title,
-// 			  map: map
-// 			});
+	//  Embed Map in #map-embed element
+	//  Then responsive resize event listener
+	function mapGen() {
+		map = new google.maps.Map(document.getElementById('map-embed'), {
+		  zoom: 7,
+		  center: map_coordinates,
+		  mapTypeId: google.maps.MapTypeId.HYBRID,
+		  scrollwheel: false
+		});
 
-// 			this_point.mark.addListener('click', function() {
-// 				updateMapMenu(this_point);
-// 			});
-// 		})
-// 	}
-
-
-// 	function updateMapMenu(post) {
-// 		$('#markTitle').text(post.title);
-// 		$('#markContent').html(post.content);
-// 		$('.js-watch')
-// 			.attr({
-// 			  "data-fade-in":   post.fade_in,
-// 			  "data-video-id":  post.id,
-// 			  "data-video-url": post.url
-// 			}); 
-
-// 		$('#mapInfoDisplay *:hidden').hide().removeClass('hidden').fadeIn(650);
-// 	};
+		google.maps.event.addDomListener(window, "resize", function() {
+		  var center;
+		  center = map.getCenter();
+		  google.maps.event.trigger(map, "resize");
+		  map.setCenter(center);
+		});
+	}
 
 
+	//  For each post in map_post
+	//  create and store a new Google Maps Marker
+	function marksGen() { 
+		map_posts.forEach(function(point) {
+			var this_point = point;
+			this_point.mark = new google.maps.Marker({
+			  position: point.coordinates,
+			  title: point.title,
+			  map: map
+			});
 
-// 	const publicApi = {
-// 		init:  init(),
-// 		posts: map_posts
-// 	};
-
-// 	return { publicApi }
-// }
+			this_point.mark.addListener('click', function() {
+				updateMapMenu(this_point);
+			});
+		})
+	}
 
 
-// //  Called when Google Map CDN is compvare
-// function initMap() {
-// 	console.log('initMap');
-// 	WebdocMap().init('/videos.json');
-// };
+	function updateMapMenu(post) {
+		$('#markTitle').text(post.title);
+		$('#markContent').html(post.content);
+		$('.js-watch')
+			.attr({
+			  "data-fade-in":   post.fade_in,
+			  "data-video-id":  post.id,
+			  "data-video-url": post.url
+			}); 
+
+		$('#mapInfoDisplay *:hidden').hide().removeClass('hidden').fadeIn(650);
+	};
+
+
+
+	var publicApi = {
+		init:  init(),
+		posts: map_posts
+	};
+
+	return { publicApi }
+}
+
+
+//  Called when Google Map CDN is compvare
+function initMap() {
+	console.log('initMap');
+	WebdocMap().init('/videos.json');
+};
--- app/assets/javascripts/player/interactive_player.js
+++ app/assets/javascripts/player/interactive_player.js
@@ -1,142 +1,142 @@
-// function WebdocPlayer() {
+function WebdocPlayer() {
 
-//     // DONT FORGET TO USE THIS ONE
-//     // var video_id = $('#player-container').data('video-id');
-//     var video_id = 1;
+    // DONT FORGET TO USE THIS ONE
+    // var video_id = $('#player-container').data('video-id');
+    var video_id = 1;
 
-//     const player = { 
-//         youtube: {},
-//         playing: false,
-//         interval: {},
-//         current_episode: {},
-//         stopButtons:  function stop() {
-//           clearInterval(this.interval);
-//         },
-//         activateButtons:  function start(start_time) { 
-//             var time = start_time;
-//             this.interval = setInterval(function() { 
-//               drawButtons(time);
-//               time ++;
-//               },1000);
-//         }
-//     }
+    var player = { 
+        youtube: {},
+        playing: false,
+        interval: {},
+        current_episode: {},
+        stopButtons:  function stop() {
+          clearInterval(this.interval);
+        },
+        activateButtons:  function start(start_time) { 
+            var time = start_time;
+            this.interval = setInterval(function() { 
+              drawButtons(time);
+              time ++;
+              },1000);
+        }
+    }
 
-//     // episode = ~rb~Video.find(video_id)
-//     // video_id = closure #player-container
-//     function init() {
-//         fetch("/videos/" + video_id + ".json")
-//             .then(function(response) { 
-//                 return response.json()
-//             })
-//             .then(function(episode) {
-//                 player.current_episode = episode;
-//                 console.log(player.current_episode.url);
-//                 youtubeGen(player.current_episode.url);
-//             });
-//     };
+    // episode = ~rb~Video.find(video_id)
+    // video_id = closure #player-container
+    function init() {
+        fetch("/videos/" + video_id + ".json")
+            .then(function(response) { 
+                return response.json()
+            })
+            .then(function(episode) {
+                player.current_episode = episode;
+                console.log(player.current_episode.url);
+                youtubeGen(player.current_episode.url);
+            });
+    };
 
-//     // onPlayerReady: Add onClick to .js-watch
-//     // onPlayerStateChange: Handles button drawing
-//     function youtubeGen(url) {
-//         player.youtube = new YT.Player('youtube', {
-//             videoId: url,
-//             playerVars: { fs: false },
-//             events: {
-//             'onReady': onPlayerReady, 
-//             'onStateChange': onPlayerStateChange
-//             }
-//         });
-//     };
+    // onPlayerReady: Add onClick to .js-watch
+    // onPlayerStateChange: Handles button drawing
+    function youtubeGen(url) {
+        player.youtube = new YT.Player('youtube', {
+            videoId: url,
+            playerVars: { fs: false },
+            events: {
+            'onReady': onPlayerReady, 
+            'onStateChange': onPlayerStateChange
+            }
+        });
+    };
 
-//     // .js-watch events
-//     function onPlayerReady() {
-//         $('.js-watch').on('click', function() {
-//             button = $(this);
-//             var fade_in   = button.data('fadeIn');
-//             var video_id  = button.data('videoId');
-//             var video_url = button.data('videoUrl');
+    // .js-watch events
+    function onPlayerReady() {
+        $('.js-watch').on('click', function() {
+            button = $(this);
+            var fade_in   = button.data('fadeIn');
+            var video_id  = button.data('videoId');
+            var video_url = button.data('videoUrl');
 
-//             changeVideo(video_id, video_url, fade_in);
-//         });
-//     };
+            changeVideo(video_id, video_url, fade_in);
+        });
+    };
 
-//     // Handles button drawing functions
-//     function onPlayerStateChange(event) {
-//         if (event.data == 1) {
-//             if (!player.playing) {
-//                 $("#main-navbar").fadeToggle();
-//                 player.playing = true;
-//                 current_time = player.youtube.getCurrentTime();
-//                 player.activateButtons(current_time);
-//             }
-//         }
+    // Handles button drawing functions
+    function onPlayerStateChange(event) {
+        if (event.data == 1) {
+            if (!player.playing) {
+                $("#main-navbar").fadeToggle();
+                player.playing = true;
+                current_time = player.youtube.getCurrentTime();
+                player.activateButtons(current_time);
+            }
+        }
 
-//         if (event.data == 2) {
-//             $("#main-navbar").fadeToggle();
-//             player.playing = false;
-//             player.stopButtons();
-//         }
-//     };
+        if (event.data == 2) {
+            $("#main-navbar").fadeToggle();
+            player.playing = false;
+            player.stopButtons();
+        }
+    };
 
-//     // Fetches new video into player.current_episode
-//     // Load new player.youtube
-//     function changeVideo(video_id, video_url, fade_in) {
-//         var video_post_url = "/videos/" + video_id + "/video_posts.json";
-//         var   map_post_url = "/videos/" + video_id + "/map_posts.json";
+    // Fetches new video into player.current_episode
+    // Load new player.youtube
+    function changeVideo(video_id, video_url, fade_in) {
+        var video_post_url = "/videos/" + video_id + "/video_posts.json";
+        var   map_post_url = "/videos/" + video_id + "/map_posts.json";
 
-//         fetch(video_post_url)
-//             .then(function(response) { 
-//                 return response.json();
-//             })
-//             .then(function(posts_json) {
-//                 player.youtube.loadVideoById(video_url);
-//                 player.current_episode.video_posts = posts_json;
-//             })
-//     };
-
-
-
-
-
-//     // TODO join map_posts + video_posts
-//     function drawButtons(current_time) {
-//         var posts = [];
-//         player.current_episode.posts.forEach(function(post) {
-//             var fade_in = post.fade_in;
-//             var fade_out = post.fade_out;
-//             if (current_time >= fade_in &&
-//                 current_time <= fade_out) {
-//                 console.log(fade_in);
-//             }
-
-//             if (current_time < fade_in &&
-//                 current_time > fade_out) {
-//                 console.log(fade_in);
-//             }
-
-
-//         });
-//     }
+        fetch(video_post_url)
+            .then(function(response) { 
+                return response.json();
+            })
+            .then(function(posts_json) {
+                player.youtube.loadVideoById(video_url);
+                player.current_episode.video_posts = posts_json;
+            })
+    };
 
 
 
 
 
+    // TODO join map_posts + video_posts
+    function drawButtons(current_time) {
+        var posts = [];
+        player.current_episode.posts.forEach(function(post) {
+            var fade_in = post.fade_in;
+            var fade_out = post.fade_out;
+            if (current_time >= fade_in &&
+                current_time <= fade_out) {
+                console.log(fade_in);
+            }
+
+            if (current_time < fade_in &&
+                current_time > fade_out) {
+                console.log(fade_in);
+            }
 
 
-//     const publicAPI = {
-//         init: init(), 
-//         changeVideo: changeVideo(),
-//     };
-
-//     return  { publicAPI };
-// };
+        });
+    }
 
 
 
 
-// function onYouTubeIframeAPIReady() {
-//     console.log('onYoutubeAPIReady');
-//     WebdocPlayer().init();
-// };
+
+
+
+    var publicAPI = {
+        init: init(), 
+        changeVideo: changeVideo(),
+    };
+
+    return  { publicAPI };
+};
+
+
+
+
+function onYouTubeIframeAPIReady() {
+    console.log('onYoutubeAPIReady');
+    WebdocPlayer().init();
+};
 
