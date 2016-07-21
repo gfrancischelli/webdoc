f1ab0812d1440c90a65c5ac5c882f7f9c44fa07e
Giovani Francischelli <gifrancischelli@gmail.com>
2016-07-21 17:54:10 -0300

    simple create video action

---
+++ app/assets/javascripts/webdoc/video_controller.js
@@ -0,0 +1,100 @@
+function VideoController() {
+
+    var episode = {};
+
+    function init() {
+        console.log('Video.init()');
+    };
+
+    function load(video_id) {
+        fetch(`/videos/${video_id}.json`)
+            .then(function(response) { return response.json() })
+            .then(function(episode_json) {
+                episode = episode_json;
+            });
+        return episode;
+    };
+
+    return { 
+        init: init,
+        load: load,
+    }
+};
+
+var app = (function(){
+    var iMap = WebdocMap();
+    var Player = WebdocPlayer();
+    var Video = VideoController();
+
+    var is_ready = false;
+    var video_id = 1;
+    var episode = {hey: 'hey'};
+
+    function init() {
+        loadEpisode(video_id);
+    }
+
+    function ready() {
+        return is_ready
+    }
+
+    function loadEpisode(video_id) {
+        episode = Video.load(video_id);
+        is_ready = true;
+    }
+
+    function initPlayer() {
+        fetch(`/videos/${video_id}.json`)
+            .then(function(response) { return response.json() })
+            .then(function(episode_json) {
+                episode = episode_json;
+                Player.init(episode);
+            });
+    }
+
+    var publicApi = {
+        ready: ready,
+        initPlayer: init,
+        loadEpisode: loadEpisode,
+    }
+
+    return { publicApi }
+})();
+
+var teste = (function(){
+    var iMap = WebdocMap();
+    var Video = VideoController();
+    var Player = WebdocPlayer();
+
+    var episode = {};
+
+    var name = 'nomedejesus';
+
+    function initDocMap() {
+        iMap.init();
+    }
+
+    function initPlayer(video_id) {
+        fetch(`/videos/${video_id}.json`)
+            .then(function(response) { return response.json() })
+            .then(function(episode_json) {
+                episode = episode_json;
+                Player.init(episode);
+            });
+    }
+
+    return { 
+        initDocMap: initDocMap,
+        initPlayer: initPlayer,
+    }
+})();
+
+function onYouTubeIframeAPIReady() {
+    console.log('onYouTubeIframeAPIReady()')
+    teste.initPlayer(1);
+};
+
+function initMap() {
+    console.log('initMap()')
+    teste.initDocMap();
+};
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -10,9 +10,21 @@ class VideosController < ApplicationController
 		@video = Video.find(params[:id])
 	end
 
+	def new
+		@video = Video.new()
+	end
+
+	def create
+		@video = Video.create(video_params)
+	end
+
 	private
 
 		def set_video
 			@video = Video.find(params[:id])
+		end
+
+		def video_params
+			params.require(:video).permit(:title, :url)
 		end
 end
